1b0958ab7576f3cfdc57858543fa6c241b6da20d
oclarke <oliverclarke1610@gmail.com>
2025-09-19 10:55:44 +0100

    Removed unneeded folder

--- managingstaff/src/main/java/com/example/managingstaff/infrastructure/StaffRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.managingstaff.infrastructure;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StaffRepository extends JpaRepository<StaffJpa, String> {
-}
--- managingstaff/src/main/java/com/example/managingstaff/infrastructure/events/EventStoreRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.managingstaff.infrastructure.events;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EventStoreRepository extends JpaRepository<EventStoreJpa, Integer> {
-}
